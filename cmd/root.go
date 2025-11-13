825312e032da037d9efbc747d4f1a547430bef58
mattsharp0 <matthew.sharp3@gmail.com>
2025-11-13 10:23:37 -0700

    misc: updated root message

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ var rootCmd = &cobra.Command{
 	Short: "ToDo is a CLI based ToDo list written in Go",
 	Long:  "A simple, lightweight ToDo list app that lives in the CLI",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Welcome to Go-ToDo, a CLI based ToDo list application written in Go.")
+		fmt.Println("ToDo is a CLI based ToDo list written in Go.\nRun 'todo list' to show todos,'todo add <item>' to add a todo or 'todo help' for more infomration")
 	},
 }
 
