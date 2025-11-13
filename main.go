02c6b8bf70b159725e403224c1df4a1eb1955d71
mattsharp0 <matthew.sharp3@gmail.com>
2025-11-13 08:57:45 -0700

    feat: create ToDo list csv with directory name

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 	"todo-list-cli/cmd"
 	"todo-list-cli/internal"
 )
 
 func main() {
-	ToDoList := internal.InitToDoList("ToDos.csv")
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Unable to get current working directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	dirName := filepath.Base(wd)
+	dirName = strings.ReplaceAll(dirName, " ", "_")
+	dirName = strings.ReplaceAll(dirName, "/", "_")
+	dirName = strings.ReplaceAll(dirName, "\\", "_")
+
+	filename := dirName + "_ToDoList.csv"
+
+	ToDoList := internal.InitToDoList(filename)
 	cmd.SetToDoList(ToDoList)
 	cmd.Execute()
 }
